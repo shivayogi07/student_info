48fc5530c39ce1347d530ad90bedc9b6ae53c7f8
shivu <shivayogids5@gmail.com>
2026-03-22 10:41:21 +0530

    created the website for impire hotel

--- app3.js
+++ app3.js
@@ -1,12 +1,29 @@
-let input = document.querySelector("#text");
-let para = document.querySelector("p");
+// let input = document.querySelector("#text");
+// let para = document.querySelector("p");
+// let body = document.querySelector("body")
 
-input.addEventListener("change", function(){
-    console.log(this.value);
-    para.innerText = this.value;
-});
+// input.addEventListener("change", function(){
+//     console.log(this.value);
+//     para.innerText = this.value;
+// });
 
-input.addEventListener("input", function(){
-    console.log(this.value);
-    para.innerText = this.value;
-});
+// input.addEventListener("input", function(){
+//     console.log(this.value);
+//     para.innerText = this.value;
+// });
+
+// let btm = document.createElement("button");
+// body.appendChild(btm);
+
+// btm.innerText = "click me!";
+
+// btm.addEventListener("click",changecolor);
+
+// function changecolor(){
+//     this.style.color = "green";
+// };
+
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url);
